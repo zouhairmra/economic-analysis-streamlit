4070415b3f28d967a88a963b60b899208ea271b7
zouhairmra <zouhairmrabet@gmail.com>
2025-07-17 09:06:50 +0300

    Create 2_Forecasting.py
    Signed-off-by: zouhairmra <zouhairmrabet@gmail.com>

---
+++ pages/2_Forecasting.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import plotly.express as px
+
+st.header("Forecasting")
+uploaded = st.file_uploader("Upload CSV with year + target", type="csv")
+if uploaded:
+    df = pd.read_csv(uploaded)
+    if 'year' not in df.columns or df.shape[1] < 2:
+        st.error("Need a 'year' column + one numeric column.")
+    else:
+        ycol = st.selectbox("Choose target:", df.columns.drop('year'))
+        model = LinearRegression()
+        model.fit(df[['year']], df[ycol])
+        df['predicted'] = model.predict(df[['year']])
+        fig = px.line(df, x='year', y=[ycol, 'predicted'])
+        st.plotly_chart(fig)
+        st.write(df[['year', ycol, 'predicted']])
